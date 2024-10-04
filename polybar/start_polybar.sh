87c8f0cde89ca5f87f548e44a6bc9dbe10add2c6
Riken7 <rikenprajapati2005@gmail.com>
2024-10-04 20:44:15 +0530

    <>

--- polybar/start_polybar.sh
+++ polybar/start_polybar.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-echo "starting polybar" >>/tmp/polybar.log
-killall polybar
+pkill polybar
 
 # Launch Polybar
 polybar example &  # Replace 'example' with your bar name if different
-echo "polybar launched" >>/tmp/polybar.log
